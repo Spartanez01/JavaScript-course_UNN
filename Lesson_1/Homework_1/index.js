85713388d3ca55fc6c465087722e738ad868137f
Adis <Spartanez509@gmail.com>
2021-09-23 15:54:27 +0300

    add homework_1

---
+++ Lesson_1/Homework_1/index.js
@@ -0,0 +1,45 @@
+/* 2)Создать переменную для каждого типа данных */
+
+/* number */
+let circulationNumber = 4212;
+console.log(circulationNumber);
+console.log(typeof circulationNumber);
+console.log(typeof(circulationNumber));
+console.log(12/0);
+console.log("Apple"/12);
+
+/* string */
+const identificationCard = "passport";
+
+console.log(identificationCard)
+console.log(identificationCard);
+console.log(typeof identificationCard);
+console.log(typeof(identificationCard));
+
+/* boolean */
+let residentialAddressIsEntered = false;
+console.log(residentialAddressIsEntered);
+console.log(typeof residentialAddressIsEntered);
+console.log(typeof(residentialAddressIsEntered));
+
+/* null */
+let parkingSpace = null;
+console.log(parkingSpace);
+
+/* undefined */
+let ship;
+console.log(ship);
+
+/* object */
+let ={
+    name:"passport",
+    serial:2215,
+    number:1158697,
+}
+
+/* symbol */
+let id = Symbol("id");
+console.log(id);
+
+/* bigint */
+const bigInt = 15523255264853521522134567890n;
